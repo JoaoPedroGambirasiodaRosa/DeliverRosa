d870cecf80427f83a8db5989694552f7fa24676e
JPedro-M <joaopedromarques2323@gmail.com>
2022-10-10 14:54:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/delivery3000/Form_Login.java
@@ -0,0 +1,14 @@
+package com.example.delivery3000;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
