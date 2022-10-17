8fb57759bfbdbc50fdfedbb785415f7461e27901
JPedro-M <joaopedromarques2323@gmail.com>
2022-10-17 14:25:09 -0300

    v1.1

--- app/src/main/java/com/example/delivery3000/Form_Login.java
+++ app/src/main/java/com/example/delivery3000/Form_Login.java
@@ -2,13 +2,28 @@ package com.example.delivery3000;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Form_Login extends AppCompatActivity {
+    TextView txtCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+        getSupportActionBar().hide();
+
+        txtCadastro = findViewById(R.id.txtCriarUser);
+
+        txtCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Form_Login.this, Form_Cadastro.class);
+                startActivity(i);
+            }
+        });
     }
 }
